14049e2382aaa342730313ee8d5aa7f79f5747a2
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-06-04 23:54:44 +0500

    visibility & field of inner class

---
+++ Inner/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person=new Person();
+        Person.Adress adress=person.getAdress();
+    }
+}
--- Inner/src/Person.java
+++ Inner/src/Person.java
@@ -5,21 +5,35 @@ public class Person {
     private int age;
     private Date dateOfbirth;
     private Adress adress;
-    class Adress{//Can extend any outer class, any quantity of interface.
+    private long phone;
+     class Adress{//Can extend any outer class, any quantity of interface.
         // must not have any static object etc.
+         //if it were private the owner class could not get access to the methods
         private String country;
         private String city;
         private String house;
         private long phone;
+
         int ageGet(){//have access to owner class objects
             return age=10;
         }
+
+        public Adress() {
+        }
+
+        public Adress(long phone) {
+            this.phone=phone+Person.this.phone;//only like this you can call outer field in innner
+        }
+
         class Rooms{
             //can have inner class but it is not recommended to do
         }
     }
 
     public Person() {
+    }
+    public Adress getAdress(){//to this method
+        return adress;
     }
     public void operation(){
         adress.city="Tashkent";//the way to access inner objects of the inner class
