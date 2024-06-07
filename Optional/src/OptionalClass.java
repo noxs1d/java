4d85bd57438a7d56fa6d095c03ce2f730f214ce4
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-06-07 16:09:10 +0500

    Optional

---
+++ Optional/src/OptionalClass.java
@@ -0,0 +1,15 @@
+import java.util.Optional;
+
+public class OptionalClass {
+    public static void main(String[] args) {
+        Optional<String> optionalString= Optional.of("Value of optional");
+        optionalString.ifPresent(value -> System.out.println("-> " + value));
+        Optional<String> optionalEmpty=Optional.empty();
+        System.out.println("There is null: "+/*optionalEmpty.get() + */ optionalEmpty.orElse("Null"));
+        // u cannot call optional.get() while optional is empty, it will invoke
+        String nullstring =null;
+        Optional<String> optionalnull=Optional.ofNullable(nullstring);
+        System.out.println(optionalnull.or(()->Optional.of("null value or this value")));
+    }
+
+}
