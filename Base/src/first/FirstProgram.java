26c8b10b1a330bc0af46e4f8683f487fc673a9f4
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-06-11 01:42:23 +0500

    First Program

--- Base/src/first/FirstProgram.java
+++ Base/src/first/FirstProgram.java
@@ -1,7 +1,17 @@
 package first;
 
+import java.io.IOException;
+
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x;
+        try {
+            x=System.in.read();//like this u can enter only character but with Scanner class u can enter the line
+            System.out.println("Code: "+ x+" \t value: "+(char)x);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
