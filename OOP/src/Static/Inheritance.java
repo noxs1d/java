07cea96fa6ef668ae3d97cd2604d31ad0457eab3
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-06-25 23:54:37 +0500

    inherritance rule 1

---
+++ OOP/src/Static/Inheritance.java
@@ -0,0 +1,13 @@
+package Static;
+
+public class Inheritance {
+
+    Inheritance(){
+        System.out.println("Father class");
+    }
+}
+class Son extends Inheritance{
+    Son(){
+        System.out.println("Son class");
+    }
+}
---
+++ OOP/src/Static/Main.java
@@ -0,0 +1,7 @@
+package Static;
+
+public class Main {
+    public static void main(String[] args) {
+        Son son=new Son(); //chain rule #1
+    }
+}
