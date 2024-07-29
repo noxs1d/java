e044e952e22826391acf42031e4ea47d54257241
noxs1d <117533743+noxs1d@users.noreply.github.com>
2024-07-29 21:23:52 +0500

    - u -

--- String/src/strings/StringClass.java
+++ String/src/strings/StringClass.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.regex.Pattern;
+
 public class StringClass {
     public static void main(String[] args) {
         String string="    689  980   ";
@@ -9,6 +11,11 @@ public class StringClass {
         System.out.println(string.compareTo(str));
         System.out.println(string.strip());
         System.out.println(str.replaceAll("\\s+",""));
-        System.out.printf("He%do leet%s",77,"Code");
+        System.out.printf("He%do leet%s",77,"Code\n");
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.insert(0,str);
+        System.out.println(stringBuilder +" "+ stringBuilder.reverse());
+        Pattern pattern=Pattern.compile("java.lava.",Pattern.COMMENTS);
+        System.out.println(pattern.pattern()+"\n"+Pattern.matches("j.+a","java"));
     }
 }
